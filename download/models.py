deb569ca030236307c93114c102cd888102b7571
haniyeh <haniyehzahraee@gmail.com>
2022-04-27 01:23:03 +0430

    get data from database and add image filde and descripthon fild

--- download/models.py
+++ download/models.py
@@ -1,6 +1,8 @@
 from unittest import mock
 from django.db import models
 
+
+
 # Create your models here.
 
 
@@ -10,6 +12,9 @@ from django.db import models
 class FilesAdmin(models.Model):
     adminupload=models.FileField(upload_to='media')
     title=models.CharField(max_length=60)
+    discription=models.TextField()
+    downloadooption=models.CharField(max_length=100)
+    imag = models.ImageField(upload_to='media')
 
     def __str__(self) :
         return self.title
