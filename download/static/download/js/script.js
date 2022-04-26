a5b95d25ff7c05647c5f56140eae041159fa5470
Alireza <zamani802000@gmail.com>
2022-04-26 12:27:05 +0430

    chenge header and create section download

--- download/static/download/js/script.js
+++ download/static/download/js/script.js
@@ -1,39 +1,44 @@
 (function() {
-
     $(document).ready(function() {
+        const iconSearch = $("#icon-search");
+        const inputBox = $("#input-search");
+
+
+
+        iconSearch.on("click", function() {
+            inputBox.show();
+            inputBox.animate({
+                width: "260px"
+            }, 400)
+        })
+        $("#more1").click(function() {
+            $("#don1").toggleClass("active");
+        })
+        $("#more2").click(function() {
+            $("#don2").toggleClass("active");
+        })
+
+
 
         function menuscroll() {
-            let navmenu = $('.nav-menu');
+            let navmenu = $(".nav-menu");
             if ($(window).scrollTop() > 50) {
-                navmenu.addClass('is-scrolling');
+                navmenu.addClass("is-scrolling");
             } else {
-                navmenu.removeClass('is-scrolling');
+                navmenu.removeClass("is-scrolling");
             }
         }
 
         menuscroll();
-        $(window).on('scroll', menuscroll);
+        $(window).on("scroll", menuscroll);
 
-        let sideNav = $('#navbarColapse');
-        sideNav.on('show.bs.collapse', function() {
-            $(this).parents('.nav-menu').addClass('menu-is-open');
-        })
+        let sideNav = $("#navbarColapse");
+        sideNav.on("show.bs.collapse", function() {
+            $(this).parents(".nav-menu").addClass("menu-is-open");
+        });
 
-        sideNav.on('hide.bs.collapse', function() {
-            $(this).parents('.nav-menu').removeClass('menu-is-open');
-        })
+        sideNav.on("hide.bs.collapse", function() {
+            $(this).parents(".nav-menu").removeClass("menu-is-open");
+        });
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-})()
+})();
